10d3b3b4772221f7fa112b6f7538212afdf60f6f
jahatch512 <ja.hatch512@gmail.com>
2016-07-12 23:54:37 -0600

    added two more problems

--- js/practice.js
+++ js/practice.js
@@ -38,7 +38,27 @@ while (nextVal < 4000000) {
   nextVal = nextVal + val1;
   val1 = temp;
 }
+// return summ;
 
-return summ;
+//prob 4
 
-console.log(summ);
+// var dashes = function() {
+//   var output = '';
+//   var number = window.prompt("Enter a number");
+//   for (var i = 1; i <= number.length; i++) {
+//     if (number[i] === undefined){
+//       return output + number[i-1];
+//     }
+//
+//     var old = number[i-1];
+//     if ((old % 2 !== 0) && (number[i] % 2 !== 0)){
+//       output = output + number[i-1] + '-';
+//     } else {
+//       output = output + number[i-1];
+//     }
+//   }
+//   return output;
+// };
+// console.log(dashes());
+
+console.log("5" % 2);
