3ff1968a9bce36e003e9961546c15cf4a7a32887
jahatch512 <ja.hatch512@gmail.com>
2016-07-13 15:00:47 -0600

    started running intro.js in the browser

--- js/intro.js
+++ js/intro.js
@@ -1,9 +1,22 @@
 function mysteryScoping1() {
-  let x = "out of block";
+  // let x = "out of block";
   if (true) {
-    let x = "in block";
+    // let x = "in block";
     console.log(x);
   }
   console.log(x);
 }
-mysteryScoping1();
+
+function madLib(s1, s2, s3){
+  return `We shall ${s1} the ${s2} ${s3}`
+}
+
+function printChildren(parent, ...children){
+  var pString = parent + "'s children are: ";
+  console.log(pString);
+  children.forEach(function(child){
+    console.log(child);
+  })
+}
+
+console.log(printChildren("Geourg", "Cassie", "Jeff", "roger"));
