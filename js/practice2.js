15322c14dee3de76c00439585d30b94f8669ddd2
jahatch512 <ja.hatch512@gmail.com>
2016-07-20 21:45:11 -0600

    completed 10 probems in practice 2

--- js/practice2.js
+++ js/practice2.js
@@ -47,3 +47,97 @@ function missing(arr){
     inc += 1;
   }
 }
+
+function removeDup(arr){
+  var seen = {};
+  var final = [];
+
+  for (var i = 0; i < arr.length; i++){
+    if (seen[arr[i]]){
+
+    } else {
+      final.push(arr[i]);
+      seen[arr[i]] = 1;
+    }
+  }
+  return final;
+}
+
+function pig(str){
+  var words = str.split(" ");
+  var final = [];
+  var vowels = ["a", "e", "i", "o", "u"];
+  words.forEach(function(word){
+    if (vowels.indexOf(word[0]) === -1){
+      var newWord = word.slice(1) + word[0] + "ay";
+      final.push(newWord);
+    } else {
+      newWord = word + "ay";
+      final.push(newWord);
+    }
+  });
+
+  return final.join(" ");
+}
+
+function same(arr1, arr2){
+  arr1 = arr1.sort();
+  arr2 = arr2.sort();
+
+  for (var i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]){
+      return false;
+    }
+  }
+  return true;
+}
+
+var cutoff = function(arr1, max) {
+  var finalA = [];
+  arr1.forEach(function(el){
+    if (el < max){
+      finalA.push(el);
+    } else {
+      finalA.push(max);
+    }
+  });
+
+  return finalA;
+};
+
+function randTen(){
+  var over = false;
+  var answers = [];
+  var seen = {};
+  while (over === false){
+    var randy = Math.floor(Math.random() * 100);
+    if (seen[randy]){
+      continue;
+    } else {
+      seen[randy] = 1;
+      answers.push(randy);
+    }
+
+    if (answers.length === 10){
+      over = true;
+    }
+  }
+  return answers;
+}
+
+var mergeList = function(arr1, arr2){
+  var merged = [];
+  while (arr1.length > 0 && arr2.length > 0){
+    if (arr1[0] < arr2[0]){
+      merged.push(arr1.shift());
+    } else {
+      merged.push(arr2.shift());
+    }
+  }
+
+  merged = merged.concat(arr1);
+  merged = merged.concat(arr2);
+  return merged;
+};
+
+console.log(mergeList([1,2,4,6], [3,5,7]));
